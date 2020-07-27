fd8d4e2bf3f76415b9d5767dfebf9fcd0b4b7d38
jcruz97 <cruzjurgen97@gmail.com>
2020-07-27 09:47:19 +0200

    init push + first exs

---
+++ 3.1.Test1/questions.js
@@ -0,0 +1,65 @@
+
+let tailleString = (texte) => {
+    return texte.length;
+}
+let remplaceECar = (texte) => {
+    let a = texte.indexOf("e")
+    let text =  texte.substr(" ", a) + " " + texte.substr(a + 1 , texte.length);
+    return text;
+}
+let concatString = (texte1, texte2) => {
+    return texte1 + texte2;
+}
+let afficherCar5 =  (texte) => {
+    return texte[4];
+}
+let afficher9Car =  (texte) => {
+    let car = texte.substr(0,9);
+    return car;
+}
+let majusculeString =  (texte) => {
+    return texte.toUpperCase();
+}
+let minusculeString =  (texte) => {
+    return texte.toLowerCase();
+}
+let SupprEspaceString =  (texte) => {
+    let res1 = texte.replace(" ","");
+    let res2 = res1.replace("jour ", "jour");
+    return res2;
+}
+let IsString =  (texte) => {
+
+}
+
+let AfficherExtensionString =  (texte) => {
+
+}
+let NombreEspaceString =  (texte) => {
+
+}
+let InverseString =  (texte) => {
+
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+let calculPuissance =  (x, y) => {
+
+}
+let valeurAbsolue =  (nombre) => {
+
+}
+let valeurAbsolueArray =  (array) => {
+
+}
+let sufaceCercle =  (rayon) => {
+
+}
+let hypothenuse =  (ab, ac) => {
+
+}
+let calculIMC =  (poids, taille) => {
+
+}
