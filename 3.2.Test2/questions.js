9c8f4d33d86c92cd33780c82bd7dfb298a541fb2
jcruz97 <cruzjurgen97@gmail.com>
2020-07-27 12:27:50 +0200

    Jasmine 2 / 12h30 push

---
+++ 3.2.Test2/questions.js
@@ -0,0 +1,48 @@
+let CreationTableauLangages =  () => {
+    let array = ["Html","CSS","Java","PHP"];
+    return array;
+}
+
+let CreationTableauNombres =  () => {
+    let nombres = [0,1,2,3,4,5];
+    return nombres;
+    
+}
+
+let RemplacementElement =  (langages) => {
+    langages.splice(2,1,"Javascript");
+    return langages;
+}
+
+let AjoutElementLangages =  (langages) => {
+    langages.splice(4,0,"Ruby","Python");
+    return langages;
+}
+
+let AjoutElementNombres =  (nombres) => {
+
+}
+
+let SuppressionPremierElement =  (langages) => {
+
+}
+
+let SuppressionDernierElement =  (langages) => {
+
+}
+
+let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
+
+}
+
+let ConversionTableauChaine =  (langages) => {
+
+}
+
+let TriTableau =  (reseaux_sociaux) => {
+
+}
+
+let InversionTableau =  (reseaux_sociaux) =>{
+
+}
